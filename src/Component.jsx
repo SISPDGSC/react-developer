dd2b728ff06417c5bd41347e084b63eff3b21f55
CoordASL <seguridadneza.sisp@gmail.com>
2025-11-14 21:24:30 -0600

    App.js

---
+++ src/Component.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function Component() {
+    
+  
+  const [text, setText] = useState() // creamos una propiedad llamada: text
+  const [updated, setUpdated] = useState() // creamos una propiedad para el boton: updated
+
+  // a traves de la extension snippets ejecutamos la sentencia enf, para crear una funcion de flecha, retirar la sentencia export
+  const textOnChange = (event) => { // obtiene la operacion de cambio de estado a traves del OnChange del evento
+    setText(event.target.value) // obtener a traves del evento dependiendo del valor del target en el value
+  } // retiramos el export y creamos 
+
+  const buttonOnclick = () => {
+    setUpdated(text) // pasar la propiedad text
+  }
+
+  return (
+    <div>
+        <input type="text" value={text} onChange={textOnChange} /> 
+        <button onClick={buttonOnclick}>Actualizar</button>
+        <p>Texto input: {text}</p> 
+        <div>Texto actualizado: {updated}</div>
+    </div>
+  )
+}
